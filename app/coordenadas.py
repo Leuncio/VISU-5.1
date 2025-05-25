28e1d6ea2b414136b95b3eba7cab345b6728d5a1
Leuncio <leitaoshp@gmail.com>
2025-05-25 21:45:54 +0200

    integrar lista de semaforos y color con bits binarios

--- app/coordenadas.py
+++ app/coordenadas.py
@@ -1,14 +1,26 @@
 # coordenadas.py
 
 # This file contains functions for converting coordinates from meters to CSS percentage values
-from .converciones import metros_a_css_porcentaje, numero_para_lista_binaria
+from converciones import metros_a_css_porcentaje, numero_para_lista_binaria
 
-NUMERO_BINARIO = numero_para_lista_binaria
+NUMERO_DE_SEMAFOFOS = 5
 
-semaforo = metros_a_css_porcentaje(8, 8)
-semaforo["color"] = numero_para_lista_binaria(6) # Puedes cambiar esto dinámicamente luego
+def obtener_semaforo(x, y, num_semaforos=NUMERO_DE_SEMAFOFOS):
+    """
+    Obtiene las coordenadas del semáforo en formato CSS,
+    asignando un único bit a cada semáforo.
+    """
+    semaforos = []
+    binario = numero_para_lista_binaria(NUMERO_DE_SEMAFOFOS)
 
-print(semaforo)
+    for i in range(num_semaforos):
+        semaforo = metros_a_css_porcentaje(x, y)
+        semaforo["color"] = binario[i % len(binario)]  # Atribui um único bit
+        semaforo["id"] = f"semaforo-{i}"
+        semaforos.append(semaforo)
 
-avg = metros_a_css_porcentaje(5, 5)
-semaforo["color"] = "rojo"
+    return semaforos
+
+
+semaforo = obtener_semaforo(10, 20)
+print (semaforo)
